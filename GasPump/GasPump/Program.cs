3faaf6b0c43c76d14d8540fb5a6dbd62c0bdde35
shuklaprachi16 <shukla.pr@husky.neu.edu>
2016-02-22 14:09:17 -0500

    Final_commit

--- GasPump/GasPump/Program.cs
+++ GasPump/GasPump/Program.cs
@@ -15,16 +15,85 @@ namespace GasPump
 
 		static void Main(string[] args)
 		{
-			// your implementation here
-		}
+            // your implementation here
+            string gasType = "";
+            string gasAmount = "";
+            Console.Write("Please enter gas type, Q/q to quit :");
+            gasType = Console.ReadLine();
 
-		// use this method to check and see if sentinel value is entered
-		public static bool UserEnteredSentinelValue(string userInput)
+
+
+            if (UserEnteredSentinelValue(gasType))
+            {
+                Console.WriteLine("exit ");
+
+            }
+            else
+            {
+                if (UserEnteredValidGasType(gasType))
+                {
+
+                    Console.Write("Please enter gas Amount, Q/q to quit :");
+                    gasAmount = Console.ReadLine();
+
+                    if (UserEnteredSentinelValue(gasAmount))
+                    {
+                        Console.WriteLine("Exit ");
+
+                        return;
+                    }
+
+                    if (UserEnteredValidAmount(gasAmount))
+                    {
+                        char inputChar = 'X';
+                        inputChar = gasType[0];
+
+                        GasType actualGasType = GasTypeMapper(inputChar);
+                        double totalCost = 0.0f;
+
+
+                        decimal decimalGasAmount = 0;
+                        bool convertType = decimal.TryParse(gasAmount, out decimalGasAmount);
+
+
+                        double amount;
+
+                        amount = System.Convert.ToDouble(decimalGasAmount);
+                        CalculateTotalCost(actualGasType, amount, ref totalCost);
+
+
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(" InValid Amount ");
+                        Console.ReadLine();
+
+                    }
+                }
+                else
+                {
+                    Console.Write("Invalis gas type ");
+                    Console.ReadLine();
+
+                }
+            }
+        }
+
+
+        // use this method to check and see if sentinel value is entered
+        public static bool UserEnteredSentinelValue(string userInput)
 		{
 			var result = false;
+            if (userInput == null)
+            {
+                result = false;
+            }
 
-			// your implementation here
-
+           else if (userInput.Equals("Q", StringComparison.InvariantCultureIgnoreCase)) {
+                result = true;
+            }
 			return result;
 		}
 
@@ -32,48 +101,88 @@ namespace GasPump
 		// this does not conform 
 		public static bool UserEnteredValidGasType(string userInput)
 		{
-			var result = false;
+            var result = false;
 
-			// your implementation here
-			
-			return result;
-		}
+            if (userInput == null)
+            {
+                result = false;
+            }
+          else  if (userInput.Equals("r", StringComparison.InvariantCultureIgnoreCase) ||
+                    userInput.Equals("m", StringComparison.InvariantCultureIgnoreCase) ||
+                        userInput.Equals("p", StringComparison.InvariantCultureIgnoreCase) ||
+                           userInput.Equals("d", StringComparison.InvariantCultureIgnoreCase))
+            {
+                result = true;
+            }
+            return result;
+        }
 
 		// use this method to parse and check the double type entered
 		// please use Double.TryParse() method 
 		public static bool UserEnteredValidAmount(string userInput)
 		{
-			var result = false;
+            var result = false;
 
-			// your implementation here
+            decimal amount = 0;
+            bool convertType = decimal.TryParse(userInput, out amount);
+            if (convertType == true)
+                result = true;
 
-			return result;
-		}
+            return result;
+        }
 
 		// use this method to map a valid char entry to its enum representation
 		// e.g. User enters 'R' or 'r' --> this should be mapped to GasType.RegularGas
 		// this mapping "must" be used within CalculateTotalCost() method later on
 		public static GasType GasTypeMapper(char c)
 		{
-			GasType gasType = GasType.None;
+            GasType gasType = GasType.None;
+            string c1 = c.ToString();
 
-			// your implementation here
+            if (c1.Equals("P", StringComparison.InvariantCultureIgnoreCase))
+                gasType = GasType.PremiumGas;
 
-			return gasType;
-		}
+
+            if (c1.Equals("R", StringComparison.InvariantCultureIgnoreCase))
+                gasType = GasType.RegularGas;
+
+
+            if (c1.Equals("M", StringComparison.InvariantCultureIgnoreCase))
+                gasType = GasType.MidgradeGas;
+
+            if (c1.Equals("D", StringComparison.InvariantCultureIgnoreCase))
+                gasType = GasType.DieselFuel;
+
+
+            return gasType;
+        }
 
 		public static double GasPriceMapper(GasType gasType)
 		{
 			var result = 0.0;
 
-			// your implementation here
+            if (gasType.Equals(GasType.RegularGas))
+                result = 1.94;
+            else if (gasType.Equals(GasType.MidgradeGas))
+                result = 2;
 
-			return result;
+            else if (gasType.Equals(GasType.PremiumGas))
+                result = 2.24;
+
+            else if (gasType.Equals(GasType.DieselFuel))
+                result = 2.17;
+            return result;
 	}
 
-		public static void CalculateTotalCost(GasType gasType, int gasAmount, ref double totalCost)
+		public static void CalculateTotalCost(GasType gasType, double gasAmount, ref double totalCost)
 		{
-			// your implementation here
-		}
+            double gasPrice = GasPriceMapper(gasType);
+            totalCost = gasPrice * gasAmount;
+
+            Console.WriteLine(" Gas Type: " + gasType + " Gas amount " + gasAmount + " Gas Price " + gasPrice);
+            Console.WriteLine("total cost: $" + totalCost);
+            Console.WriteLine("press any key to exit");
+            Console.ReadLine();
+        }
 	}
 }
